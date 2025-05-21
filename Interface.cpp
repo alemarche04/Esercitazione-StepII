895ca75a169fafa26d4a569e7bfa9a35200de11d
Alessia <ale09.marchese@gmail.com>
2025-05-21 19:20:54 +0200

    Added error handling on user input -- tested

--- Interface.cpp
+++ Interface.cpp
@@ -10,14 +10,57 @@
 /// @brief Displays the menu and takes the user input
 void DisplayMenu()
 {
-	int user_choice = -1;
+	size_t user_choice = -1;
 	vector<Function*> Functions;
 
 	cout << endl << "######## FUNCTIONS MENU ########" << endl << endl;
 	
 	do
 	{
-		cout << endl << "Select an action:" << endl;
+		PrintActions();
+
+		cin >> user_choice;
+		
+		if (cin.fail() == true)
+		{
+			InputError(user_choice);
+		}
+		else
+		{
+			switch (user_choice)
+			{
+				case 1:
+					DisplayFunctions(Functions);
+					break;
+				case 2:
+					InsertFunctions(Functions);
+					break;
+				case 3:
+					DeleteFunction(Functions);
+					break;
+				case 4:
+					DeleteAllFunctions(Functions);
+					break;
+				case 5:
+					SelectFunction(Functions);
+					break;
+				case 0:
+					break;
+				default:
+					ErrorMessage("Invalid selection");
+					break;
+			}
+		}
+	}
+	while (user_choice != 0);
+	DeleteAllFunctions(Functions);
+	cout << endl << "bye bye" << endl;
+}
+
+/// @brief Prints the different actions the user can select
+void PrintActions()
+{
+	cout << endl << "Select an action:" << endl;
 
 		cout 
 		<< "1) Display all the functions" << endl
@@ -26,40 +69,10 @@ void DisplayMenu()
 		<< "4) Delete all functions" << endl
 		<< "5) Select a function and calculate the value" << endl
 		<< "0) Exit" << endl;
-
-		cin >> user_choice;
-
-		switch (user_choice)
-		{
-			case 1:
-				DisplayFunctions(Functions);
-				break;
-			case 2:
-				InsertFunctions(Functions);
-				break;
-			case 3:
-				DeleteFunction(Functions);
-				break;
-			case 4:
-				DeleteAllFunctions(Functions);
-				break;
-			case 5:
-				SelectFunction(Functions);
-				break;
-			case 0:
-				break;
-			default:
-				cout << "Invalid choice" << endl;
-				break;
-		}
-	}
-	while (user_choice != 0);
-	DeleteAllFunctions(Functions);
-	cout << endl << "bye bye" << endl;
 }
 
 /// @brief Displays all the functions inserted by the user
-///	@param Functions vecotr of pointers to objects of class Function
+///	@param Functions vector of pointers to objects of class Function
 void DisplayFunctions(vector<Function*>& Functions)
 {
 	if (Functions.empty())
@@ -81,178 +94,212 @@ void DisplayFunctions(vector<Function*>& Functions)
 }
 
 /// @brief Allows the user to insert a new function in the Functions vector
-///	@param Functions vecotr of pointers to objects of class Function
+///	@param Functions vector of pointers to objects of class Function
 void InsertFunctions(vector<Function*>& Functions)
 {
-	int func_choice = 0;
+	size_t func_choice = 0;
 	cout << endl << "Available functions:" << endl;
 	cout << "1) Polynomial" << endl << "2) Power" << endl << "3) Exponential" << endl;
 	cout << "Choose a function: ";
 	cin >> func_choice;
-	
-	if( func_choice == 1 )
+
+	if (cin.fail() == true)
 	{
-		Polynomial* NewPol = new Polynomial;
-
-		if(!NewPol)
-		{
-			cout << endl << "Allocation error: Memory not allocated" << endl;
-			exit(-1);
-		}
-
-		int deg;
-		int confirm = -1;
-
-		do
-		{
-			cout << endl << "Insert the degree: ";
-			cin >> deg;
-
-			double* coeff = new double[deg + 1];
-			if(!coeff)
-			{
-				cout << endl << "Allocation error: Memory not allocated" << endl;
-				exit(-1);
-			}
-
-			cout << endl << "Insert the coefficients: " << endl;
-			for (int i =  0; i <= deg; i++)
-			{
-				cout << endl << "Coefficient of x^" << i << ":";
-				cin >> coeff[i];
-			}
-
-			NewPol->SetPolynomial(coeff, (deg + 1));
-
-			do
-			{
-				cout << endl << "Is this your function?" << endl;
-				NewPol->Dump();
-				cout << endl
-				<< "1) Yes" << endl
-				<< "2) No" << endl;
-
-				cin >> confirm;
-				if (confirm != 1 && confirm != 2)
-				{
-					cout << endl << "Invalid selection" << endl;
-				}
-			} 
-			while (confirm != 1 && confirm != 2);
-
-			delete[] coeff;
-		}
-		while (confirm != 1);
-		
-		Functions.push_back(NewPol);
+		InputError(func_choice);
 	}
-
-
+	else if( func_choice == 1 )
+	{
+		InsertPolynomial(Functions);		
+	}
 	else if( func_choice == 2 )
 	{
-		Power* NewPow = new Power;
-
-		if(!NewPow)
-		{
-			cout << endl << "Allocation error: Memory not allocated" << endl;
-			exit(-1);
-		}
-
-		int confirm = -1;
-		double p_e_coeff = 0;
-		double p_k_coeff = 0;
-
-		do
-		{
-			cout << endl << "Insert the exponent: ";
-			cin >> p_e_coeff;
-			cout << endl << "Insert the coefficient: ";
-			cin >> p_k_coeff;
-			NewPow->SetPower(p_k_coeff, p_e_coeff);
-
-			do
-			{
-				cout << endl << "Is this your function?" << endl;
-				NewPow->Dump();
-				cout << endl
-				<< "1) Yes" << endl
-				<< "2) No" << endl;
-				cin >> confirm;
-				if (confirm != 1 && confirm != 2)
-				{
-					cout << endl << "Invalid selection" << endl;
-				}
-			} 
-			while (confirm != 1 && confirm != 2);
-		}
-		while (confirm != 1);
-
-		Functions.push_back(NewPow);
+		InsertPower(Functions);
 	}
-
-
 	else if ( func_choice == 3 )
 	{
-		Exponential* NewExp = new Exponential;
-
-		if(!NewExp)
-		{
-			cout << endl << "Allocation error: Memory not allocated" << endl;
-			exit(-1);
-		}
-
-		int confirm = -1;
-
-		double p_c_coeff = 0;
-		double p_k_coeff = 0;
-		double p_b_coeff = 0;
-
-		do
-		{
-			cout << endl << "Insert the exponent coefficient: ";
-			cin >> p_c_coeff;
-			cout << endl << "Insert the base: ";
-			cin >> p_b_coeff;
-			cout << endl << "Insert the base coefficient: ";
-			cin >> p_k_coeff;
-			NewExp->SetExponential(p_b_coeff, p_k_coeff, p_c_coeff);
-			do
-			{
-				cout << endl << "Is this your function?" << endl;
-				NewExp->Dump();
-				cout << endl
-				<< "1) Yes" << endl
-				<< "2) No" << endl;
-				cin >> confirm;
-				if (confirm != 1 && confirm != 2)
-				{
-					cout << endl << "Invalid selection" << endl;
-				}
-			} 
-			while (confirm != 1 && confirm != 2);
-		}
-		while (confirm != 1);
-
-		Functions.push_back(NewExp);
+		InsertExponential(Functions);
 	}
 	else
 	{
-		cout << endl << "Invalid selection" << endl;
+		ErrorMessage("Invalid selection");
 	}
 
 	cout << endl;
 }
 
+/// @brief Handles the creation of a new polynomial function
+///	@param Functions vector of pointers to objects of class Function
+void InsertPolynomial(vector<Function*>& Functions)
+{
+	Polynomial* NewPol = new Polynomial;
+
+	if(!NewPol)
+	{			
+		ErrorMessage("Memory not allocated");
+		exit(-1);
+	}
+
+	int deg;
+	size_t confirm = -1;
+
+	do
+	{
+		cout << endl << "Insert the degree: ";
+		cin >> deg;
+
+		double* coeff = new double[deg + 1];
+		if(!coeff)
+		{
+			exit(-1);
+		}
+
+		cout << endl << "Insert the coefficients: " << endl;
+		for (int i =  0; i <= deg; i++)
+		{
+			cout << endl << "Coefficient of x^" << i << ":";
+			cin >> coeff[i];
+		}
+
+		NewPol->SetPolynomial(coeff, (deg + 1));
+
+		do
+		{
+			cout << endl << "Is this your function?" << endl;
+			NewPol->Dump();
+			cout << endl
+			<< "1) Yes" << endl
+			<< "2) No" << endl;
+
+			cin >> confirm;
+			if (cin.fail() == true)
+			{
+				InputError(confirm);
+			}
+			else if (confirm != 1 && confirm != 2)
+			{
+				ErrorMessage("Invalid selection");
+			}
+		} 
+		while (confirm != 1 && confirm != 2);
+
+		delete[] coeff;
+	}
+	while (confirm != 1);
+	
+	Functions.push_back(NewPol);
+}
+
+/// @brief Handles the creation of a new power function
+///	@param Functions vector of pointers to objects of class Function
+void InsertPower(vector<Function*>& Functions)
+{
+	Power* NewPow = new Power;
+
+	if(!NewPow)
+	{
+		cout << endl << "Memory not allocated" << endl;
+		exit(-1);
+	}
+
+	size_t confirm = -1;
+	double p_e_coeff = 0;
+	double p_k_coeff = 0;
+
+	do
+	{
+		cout << endl << "Insert the exponent: ";
+		cin >> p_e_coeff;
+		cout << endl << "Insert the coefficient: ";
+		cin >> p_k_coeff;
+		NewPow->SetPower(p_k_coeff, p_e_coeff);
+
+		do
+		{
+			cout << endl << "Is this your function?" << endl;
+			NewPow->Dump();
+			cout << endl
+			<< "1) Yes" << endl
+			<< "2) No" << endl;
+			cin >> confirm;
+			if (cin.fail() == true)
+			{
+				InputError(confirm);
+			}
+			else if (confirm != 1 && confirm != 2)
+			{
+				ErrorMessage("Invalid selection");
+
+			}
+		} 
+		while (confirm != 1 && confirm != 2);
+	}
+	while (confirm != 1);
+
+	Functions.push_back(NewPow);
+}
+
+/// @brief Handles the creation of a new exponential function
+///	@param Functions vector of pointers to objects of class Function
+void InsertExponential(vector<Function*>& Functions)
+{
+	Exponential* NewExp = new Exponential;
+
+	if(!NewExp)
+	{
+		ErrorMessage("Memory not allocated");
+		exit(-1);
+	}
+
+	size_t confirm = -1;
+
+	double p_c_coeff = 0;
+	double p_k_coeff = 0;
+	double p_b_coeff = 0;
+
+	do
+	{
+		cout << endl << "Insert the exponent coefficient: ";
+		cin >> p_c_coeff;
+		cout << endl << "Insert the base: ";
+		cin >> p_b_coeff;
+		cout << endl << "Insert the base coefficient: ";
+		cin >> p_k_coeff;
+		NewExp->SetExponential(p_b_coeff, p_k_coeff, p_c_coeff);
+		do
+		{
+			cout << endl << "Is this your function?" << endl;
+			NewExp->Dump();
+			cout << endl
+			<< "1) Yes" << endl
+			<< "2) No" << endl;
+			cin >> confirm;
+			
+			if (cin.fail() == true)
+			{
+				InputError(confirm);
+			}
+			else if (confirm != 1 && confirm != 2)
+			{
+				ErrorMessage("Invalid selection");
+			}
+		} 
+		while (confirm != 1 && confirm != 2);
+	}
+	while (confirm != 1);
+
+	Functions.push_back(NewExp);
+}
+
 /// @brief Allows the user to delete a function in the Functions vector
-///	@param Functions vecotr of pointers to objects of class Function
+///	@param Functions vector of pointers to objects of class Function
 void DeleteFunction(vector<Function*>& Functions)
 {
-	int del_choice = -1;
+	size_t del_choice = -1;
 	int confirm = 0;
 
 	if (Functions.empty())
 	{
-		cout << endl << "No function available" << endl;
+		ErrorMessage("No function available");
 		return;
 	}
 
@@ -260,27 +307,13 @@ void DeleteFunction(vector<Function*>& Functions)
 	{
 		do
 		{
-			cout << endl << "Select the function to be deleted: " << endl;
-			DisplayFunctions(Functions);
-			cin >> del_choice;
-			if(del_choice < 1 || del_choice > Functions.size())
-				cout << endl << "Invalid selection" << endl;
-		} while (del_choice < 1 || del_choice > Functions.size());
-		
+			del_choice = Choose(Functions, "Select the function to be deleted:");
+		} 
+		while (del_choice < 1 || del_choice > Functions.size());
 		
 		do
 		{
-			cout << endl << "Is this the function you want to delete?" << endl;
-			Functions.at((del_choice - 1))->Dump();
-			cout << endl
-			<< "1) Yes" << endl
-			<< "2) No" << endl;
-
-			cin >> confirm;
-			if (confirm != 1 && confirm != 2)
-			{
-				cout << endl << "Invalid selection" << endl;
-			}
+			confirm = Confirm(Functions, del_choice, "Is this the function you want to delete?");
 		} 
 		while (confirm != 1 && confirm != 2);
 	}
@@ -294,7 +327,7 @@ void DeleteFunction(vector<Function*>& Functions)
 }
 
 /// @brief Deletes all the functions in the Functions vector
-///	@param Functions vecotr of pointers to objects of class Function
+///	@param Functions vector of pointers to objects of class Function
 void DeleteAllFunctions(vector<Function*>& Functions)
 {
 	if (Functions.empty())
@@ -316,16 +349,16 @@ void DeleteAllFunctions(vector<Function*>& Functions)
 }
 
 /// @brief Allows the user to select a function in the Functions vector and get the value given an input
-///	@param Functions vecotr of pointers to objects of class Function
+///	@param Functions vector of pointers to objects of class Function
 void SelectFunction(vector<Function*>& Functions)
 {
-	int sel_choice = -1;
-	int confirm = 0;
+	size_t sel_choice = -1;
+	size_t confirm = 0;
 	int in = 0;
 
 	if (Functions.empty())
 	{
-		cout << endl << "No function available" << endl;
+		ErrorMessage("No function available");
 		return;
 	}
 
@@ -333,26 +366,13 @@ void SelectFunction(vector<Function*>& Functions)
 	{
 		do
 		{
-			cout << endl << "Select the function to be used: " << endl;
-			DisplayFunctions(Functions);
-			cin >> sel_choice;
-			if(sel_choice < 1 || sel_choice > Functions.size())
-				cout << endl << "Invalid selection" << endl;
-		} while (sel_choice < 1 || sel_choice > Functions.size());
+			sel_choice = Choose(Functions, "Select the function you want to calculate the value of:");
+		} 
+		while (sel_choice < 1 || sel_choice > Functions.size());
 
 		do
 		{
-			cout << endl << "Is this the function you want to select?" << endl;
-			Functions.at((sel_choice - 1))->Dump();
-			cout << endl
-			<< "1) Yes" << endl
-			<< "2) No" << endl;
-
-			cin >> confirm;
-			if (confirm != 1 && confirm != 2)
-			{
-				cout << endl << "Invalid selection" << endl;
-			}
+			confirm = Confirm(Functions, sel_choice, "Is this the function you want to select?");
 		} 
 		while (confirm != 1 && confirm != 2);
 	}
@@ -361,4 +381,69 @@ void SelectFunction(vector<Function*>& Functions)
 	cout << endl << "Insert the value of x: " << endl;
 	cin >> in;
 	cout << endl << "The result is: " << Functions.at((sel_choice - 1))->GetValue(in) << endl;
+}
+
+/// @brief asks the user wich one of the available functions they want to select
+///	@param Functions vector of pointers to objects of class Function
+/// @param text string of text used to ask the user to select a function
+///	@return ID of the selected function
+size_t Choose(vector<Function*>& Functions, const string text)
+{
+	size_t choice;
+	cout << endl << endl << text;
+	DisplayFunctions(Functions);
+	cin >> choice;
+	if (cin.fail() == true) 
+	{
+		InputError(choice);
+    }
+	else if (choice < 1 || choice > Functions.size())
+	{
+		ErrorMessage("Invalid selection");
+	}
+	return choice;
+}
+
+/// @brief asks confirmation to the user
+///	@param Functions vector of pointers to objects of class Function
+/// @param chioce ID of the selected function
+/// @param text string of text used to ask confirmation to the user
+///	@return confirmation number
+size_t Confirm(vector<Function*>& Functions, size_t choice, const string text)
+{
+	int confirm;
+	cout << endl << text << endl;
+	Functions.at((choice - 1))->Dump();
+	cout << endl
+	<< "1) Yes" << endl
+	<< "2) No" << endl;
+
+	cin >> confirm;
+	if (cin.fail() == true)
+	{
+		InputError(choice);
+	}
+	else if (confirm != 1 && confirm != 2)
+	{
+		ErrorMessage("Invalid selection");
+	}
+	return confirm;
+}
+
+
+/// @brief writes an error message 
+/// @param error message to be printed
+void ErrorMessage(const string error)
+{
+	cout << endl << "ERROR: " << error << endl;
+}
+
+
+/// @brief handles invalid input from the user
+void InputError(size_t input)
+{
+	cout << "Invalid selection" << endl;
+	cin.clear(); 
+	cin.ignore(256, '\n');
+	input = -1;
 }
--- Interface.h
+++ Interface.h
@@ -15,11 +15,42 @@
 #include "CExponential.h"
 using namespace std;
 
+/// @name Display and print
+/// @{
 void DisplayMenu();
+void PrintActions();
 void DisplayFunctions(vector<Function*>& Functions);
+/// @}
+
+/// @name Insertion
+/// @{
 void InsertFunctions(vector<Function*>& Functions);
+void InsertPolynomial(vector<Function*>& Functions);
+void InsertPower(vector<Function*>& Functions);
+void InsertExponential(vector<Function*>& Functions);
+/// @}
+
+/// @name Deletion
+/// @{
 void DeleteFunction(vector<Function*>& Functions);
 void DeleteAllFunctions(vector<Function*>& Functions);
+/// @}
+
+/// @name Selection
+/// @{
 void SelectFunction(vector<Function*>& Functions);
+/// @}
+
+/// @name Choice and confirmation
+/// @{
+size_t Choose(vector<Function*>& Functions, const string text);
+size_t Confirm(vector<Function*>& Functions, size_t sel_choice, const string text);
+/// @}
+
+/// @name Error Handling
+/// @{
+void ErrorMessage(const string error);
+void InputError(size_t input);
+/// @}
 
 #endif
